bd1cc10cc057e4aac3fc901bf7271045cf8d59d3
Marwaaz Notary <marwaaznotary1@gmail.com>
2026-08-04 19:58:20 +0300

    Configure next build for Vercel deployment

--- db/index.ts
+++ db/index.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import { env } from "cloudflare:workers";
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
--- worker/index.ts
+++ worker/index.ts
@@ -2,6 +2,9 @@
 import { handleImageOptimization, DEFAULT_DEVICE_SIZES, DEFAULT_IMAGE_SIZES } from "vinext/server/image-optimization";
 import handler from "vinext/server/app-router-entry";
 
+type Fetcher = any;
+type D1Database = any;
+
 interface Env {
   ASSETS: Fetcher;
   DB: D1Database;
